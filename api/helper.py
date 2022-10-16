a43473f240b6b5a6a5bdcfb0e9f89c3f876bce3c
nikhilkotiya8 <nikhilkotiya8@gmail.com>
2022-10-16 15:55:17 +0530

    add_pagination

--- api/helper.py
+++ api/helper.py
@@ -24,6 +24,24 @@ def get_hash_key(key: str):
     """
     hashed_key = hashlib.sha1(key.encode('utf-8')).hexdigest()
     return hashed_key
+from urllib import parse as url_parse
+def replace_query_param(url, key, val):
+    """
+    Given a URL and a key/val pair, set or replace an item in the query
+    parameters of the URL, and return the new URL.
+    """
+    result = url_parse.urlparse(url)
+    query_dict = url_parse.parse_qs(result.query, keep_blank_values=True)
+    query_dict[key] = [val]
+    query = url_parse.urlencode(sorted(list(query_dict.items())), doseq=True)
+    return url_parse.urlunsplit((result.scheme, result.netloc, result.path, query, result.fragment))
 
-
-    
+def get_next_prev_url(url, page_no, result_count, page_len):
+    page_param = 'page_no'
+    max_page_no = int(result_count / page_len) + int(result_count % page_len != 0)
+    next_link = prev_link = None
+    if max_page_no > page_no:
+        next_link = replace_query_param(url, page_param, page_no + 1)
+    if page_no > 1:
+        prev_link = replace_query_param(url, page_param, page_no - 1)
+    return next_link, prev_link
