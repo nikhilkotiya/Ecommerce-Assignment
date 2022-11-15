168d9d8d754e3cdfd6628cb3dc47f27e6e960dbc
nikhilkotiya8 <nikhilkotiya8@gmail.com>
2022-11-15 22:48:22 +0530

    allowed host change

--- ecommerce/settings.py
+++ ecommerce/settings.py
@@ -141,11 +141,7 @@ DEFAULT_FROM_EMAIL = EMAIL_HOST_USER
 
 CACHES = {
     "default": {
-        "BACKEND": "django_redis.cache.RedisCache",
-        "LOCATION": "redis://127.0.0.1:6379/1",
-        "OPTIONS": {
-            "CLIENT_CLASS": "django_redis.client.DefaultClient"
-        },
+        "BACKEND": "channels.layers.InMemoryChannelLayer"
     }
 }
 REDIS_HOST = 'localhost'
